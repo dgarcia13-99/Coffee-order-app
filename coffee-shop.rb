27656802579af96290ec10ab26a13740eff4f7a1
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-07-11 19:45:47 +0000

    Updated choice options for instance methods

--- coffee-shop.rb
+++ coffee-shop.rb
@@ -1,26 +1,59 @@
 require_relative "coffee"
 
 class Coffee_Shop
-  drink = ["1."]
-  food = ["1."]
-  flavors= ["1."]
-
+  
   def initialize
     puts "Welcome to the Grand opening of Sage Coffee Shop!"
     puts "We offer delicious breakfast food and different coffee options"
+    puts "What would you like to do?"
+    puts ["1. View Menu", "2. Place Order", "3. View Order Status"]
+    what_to_do= gets.chomp.to_i
+    if what_to_do == 1
+      view_menu
+    elsif what_to_do == 2
+      place__order
+    elsif what_to_do == 3
+      status
+    else
+      puts "Please choose a valid option"
+    end
+  end
+    
+  def view_menu
+#---------------------------------------------------------------------------------------------------------------------------------------------
+  #Menu Hashes to access items
+  drink = {:1=>"latte", :2=>"cappuccino", :3=>"americano", :4=>"macchiato", :5=>"matcha"}
+  food = {:1=>"plain bagel with cream cheese", :2=>"egg and potatoes breakfast burrito", :3=>"avocado toast", :4=>"crossaint breakfast sandwich", :5=>"grilled cheese"}
+  flavors= {:1=>"caramel", :2=>"vanilla", :3=>"butterscotch", :4=> "chocolate", :5=>"blueberry"}
+
+  #Display Menu
+  drink_menu = ["1.latte", "2.cappuccino", "3.americano", "4.macchiato", "5.matcha"]
+  food_menu = ["1.plain bagel with cream cheese", "2.egg and potatoes breakfast burrito", "3.avocado toast", "4.crossaint breakfast sandwich", "5.grilled cheese"]
+  flavor_menu = ["caramel", "vanilla", "butterscotch", "chocolate", "blueberry"]
+#----------------------------------------------------------------------------------------------------------------------------------------------
     puts "To start your order, would you like to view our food menu or coffee menu?"
     puts "Type 'food' for the food menu and 'coffee' for the coffee menu"
     choose_menu=gets.chomp
     puts "Perfect! Here is the #{choose_menu} menu"
-    puts drinks
+    display_menu= choose_menu.to_s + "_menu"
+    puts display_menu
 
-  def view_menu
-
-
+    puts "Now that you've viewed the #{choose_menu} menu, what would you like to do now?"
+    puts ["1. View Menu", "2. Place Order", "3. View Order Status"]
+    what_to_do= gets.chomp.to_i
+    if what_to_do == 1
+      view_menu
+    elsif what_to_do == 2
+      place__order
+    elsif what_to_do == 3
+      status
+    else
+      puts "Please choose a valid option"
+    end
   end
   
   
-  def place_drink_order
+  def place__order
     puts "What drink can I get for you today?"
     drink_order= gets.chomp
     puts "Would you like any flavors in your drink? Type 'Y' for 'Yes' and 'N' for 'No'"
--- coffee.rb
+++ coffee.rb
@@ -1,8 +1,9 @@
 class Coffee
-  attr_accessor :drink, :size
+  attr_accessor :drink, :temperature, :size
 
-  def initialize(drink, size)
+  def initialize(drink, temperature, size)
     @drink= drink
+    @temperature = temperature
     @size= size
   end
 
